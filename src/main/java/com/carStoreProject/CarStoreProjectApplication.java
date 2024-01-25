79eebd4ea3b571060c6e217b558192465582d655
ilazarova <lazarova_vt@abv.bg>
2024-01-25 18:56:43 +0200

    Create CarStoreProjectApplication.java

---
+++ src/main/java/com/carStoreProject/CarStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.carStoreProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStoreProjectApplication.class, args);
+	}
+
+}
