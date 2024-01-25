e03b0162ac719862a53d527df5318a5a8351d373
ilazarova <lazarova_vt@abv.bg>
2024-01-25 18:57:08 +0200

    Create CarStoreProjectApplicationTests.java

---
+++ src/test/java/com/carStoreProject/CarStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carStoreProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
